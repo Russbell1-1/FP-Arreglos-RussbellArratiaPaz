aecd54c50223458d916b099e95b7a062f39318b6
Russbell1-1 <zvalkiria@outlook.es>
2025-06-23 12:02:33 -0500

    Ejercicio 6 avance

---
+++ Ejercicio 6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,a[100],s1=0,s2=0;
+	cout<<"Cantidad de elementos: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Valor "<<i<<": ";
+		cin>>a[i];
+		if(i%2==0)s1+=a[i];
+		else s2+=a[i];
+	}
+	
+	return 0;
+}
