8891fb3e7b89f6ace080ca201d8c87d2d271485e
Russbell1-1 <zvalkiria@outlook.es>
2025-06-23 12:24:19 -0500

    Ejercicio 11 funcionamiento

---
+++ Ejercicio 11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,a[100],p=0,q=0;
+	cout<<"Cantidad de elementos: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Valor "<<i<<": ";
+		cin>>a[i];
+		if
+		(a[i]>0)p+=a[i];
+		else if
+		(a[i]<0)q+=a[i];
+	}
+	
+}
