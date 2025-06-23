62f1620f4e571fac442319e7164975c39fba2b1c
Russbell1-1 <zvalkiria@outlook.es>
2025-06-23 12:10:51 -0500

    Ejercicio 7 avance

---
+++ Ejercicio 7.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,a[100];
+	cout<<"Cantidad de elementos: ";
+	cin>>n;
+	for(int i=0;i<n;i++){
+		cout<<"Valor "<<i<<": ";
+		cin>>a[i];
+	}
+	
+}
