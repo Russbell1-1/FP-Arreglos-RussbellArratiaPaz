6e0385bd2a4d393a370a713acfe978423b4624c6
Russbell1-1 <zvalkiria@outlook.es>
2025-06-23 10:45:33 -0500

    Ejercicio 1 Final

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 int main(){
 	char Oracion[1000];
+	
 	cout<<"Ingrese la oracion: ";
-	cin.getline(Oracion,100);
+	cin.getline(Oracion,1000);
 	
 	Oracion[0]=toupper(Oracion[0]);
 	
-	for (int i=1;i<=1000;i++){
-		if (Oracion[i]=' '){
+	for (int i=1;Oracion[i]!='\0';i++){
+		if (Oracion[i]==' '){
 			Oracion[i+1]=toupper(Oracion[i+1]);
 		}
 	}
