05d2611db8f0cabf4ebc3c92d08843835970a6dc
Russbell1-1 <zvalkiria@outlook.es>
2025-06-23 10:42:01 -0500

    Ejercicio 1 avance

---
+++ Ejercicio 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+
+int main(){
+	char Oracion[1000];
+	cout<<"Ingrese la oracion: ";
+	cin.getline(Oracion,100);
+	
+	Oracion[0]=toupper(Oracion[0]);
+	
+	for (int i=1;i<=1000;i++){
+		if (Oracion[i]=' '){
+			Oracion[i+1]=toupper(Oracion[i+1]);
+		}
+	}
+	
+	cout<<Oracion<<endl;
+	
+	return 0;
+}
